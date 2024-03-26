c504906b433092e07d506b76a5db37caa01e1563
Starttoaster <b.butler@chia.net>
2024-03-26 00:00:31 -0700

    Add some base test data and a test

--- nodes_test.go
+++ nodes_test.go
@@ -30,3 +30,26 @@ func TestGetNodes(t *testing.T) {
 	require.NotNil(t, resp)
 	require.Equal(t, want, *r)
 }
+
+func TestGetNode(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("nodes/srv1", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err := fmt.Fprint(w, fixture("nodes/get_node_status.json"))
+		if err != nil {
+			return
+		}
+	})
+
+	want := GetNodeStatusResponse{
+		Data: GetNodeStatusData{},
+	}
+
+	r, resp, err := client.Nodes.GetNodeStatus("srv1")
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, *r)
+}
