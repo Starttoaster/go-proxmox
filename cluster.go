21d4dc8cc6a0206aa41944e5a9e97d0d0fea3240
Starttoaster <b.butler@chia.net>
2024-05-10 22:44:35 -0700

    Add GET cluster/resources API Method

--- cluster.go
+++ cluster.go
@@ -43,3 +43,56 @@ func (s *ClusterService) GetClusterStatus() (*GetClusterStatusResponse, *http.Re
 
 	return d, resp, nil
 }
+
+// GetClusterResourcesResponse contains the response for the /cluster/resources endpoint
+type GetClusterResourcesResponse struct {
+	Data []GetClusterResourcesData `json:"data"`
+}
+
+// GetClusterResourcesData contains data of a cluster's resources from GetClusterResources
+type GetClusterResourcesData struct {
+	ID         string   `json:"id"`
+	Node       string   `json:"node"`
+	Status     string   `json:"status"`
+	Type       string   `json:"type"`
+	CPU        *float64 `json:"cpu"`
+	Disk       *int     `json:"disk"`
+	DiskRead   *int     `json:"diskread"`
+	DiskWrite  *int     `json:"diskwrite"`
+	MaxCPU     *int     `json:"maxcpu"`
+	MaxDisk    *int     `json:"maxdisk"`
+	MaxMem     *int     `json:"maxmem"`
+	Mem        *int     `json:"mem"`
+	Name       *string  `json:"name"`
+	NetIn      *int     `json:"netin"`
+	NetOut     *int     `json:"netout"`
+	Template   *int     `json:"template"`
+	Uptime     *int     `json:"uptime"`
+	VMID       *int     `json:"vmid"`
+	HAState    *string  `json:"hastate"`
+	CgroupMode *int     `json:"cgroup-mode"`
+	Level      *string  `json:"level"`
+	Content    *string  `json:"content"`
+	PluginType *string  `json:"plugintype"`
+	Shared     *int     `json:"shared"`
+	Storage    *string  `json:"storage"`
+	SDN        *string  `json:"sdn"`
+}
+
+// GetClusterResources makes a GET request to the /cluster/resources endpoint
+// https://pve.proxmox.com/pve-docs/api-viewer/index.html#/cluster/resources
+func (s *ClusterService) GetClusterResources() (*GetClusterResourcesResponse, *http.Response, error) {
+	u := "cluster/resources"
+	req, err := s.client.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	d := new(GetClusterResourcesResponse)
+	resp, err := s.client.Do(req, d)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return d, resp, nil
+}
